cf9e0cb0c0cfdd9de86e573a33bc06add461f4d7
tobylunt <toby.lunt@gmail.com>
2018-09-11 20:37:06 -0500

    finishing compilation step - complete scrape v1.0

--- scrape_birth.py
+++ scrape_birth.py
@@ -6,6 +6,9 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import os
+import pandas as pd
+import numpy as np
+import glob
 
 # specify the url
 base_url = 'https://www.hockey-reference.com/friv/birthplaces.cgi'
@@ -27,7 +30,7 @@ for link in grid.find_all('a'):
     names += [link.string]
 
 # make a directory for individual webpages
-newpath = r'tempdata' 
+newpath = 'tempdata' 
 if not os.path.exists(newpath):
     os.makedirs(newpath)
     
@@ -50,7 +53,7 @@ for i in range(0, len(links)):
     soup = BeautifulSoup(page.text, 'html.parser')
 
     # cull the multicolumn super-headers (e.g. goalie stats)
-    for tr in soup.find_all("tr", {'class':'over_header'}): 
+    for tr in soup.find_all('tr', {'class':'over_header'}): 
         tr.decompose()
 
     # identify table
@@ -60,17 +63,51 @@ for i in range(0, len(links)):
     thead = soup.find('thead')
     tbody = soup.find('tbody')
 
-    # find header rows
+    # find header rows 
     headers = [header.text for header in thead.find_all('th')]
 
-    # data rows
+    # cull the mid-table reminder headers from the table body
+    for tr in tbody.find_all('tr', {'class':'thead'}): 
+        tr.decompose()
+
+    # data rows - need to remove "thead rowSum" classes, which are mid-table header rows
     rows = []
     for row in tbody.find_all('tr'):
         rows.append([val.text for val in row.find_all(['th','td'])])
 
-    # write to csv
+    # write to csv, remove old csv
     with open('tempdata/%s.csv' % place, 'w') as f:
         writer = csv.writer(f)
         writer.writerow(headers)
         writer.writerows(row for row in rows if row)
 
+# write in a variable including the state name to all CSVs, and append all. first loop over all files
+for filename in os.listdir(newpath):
+    
+    # read in the csv
+    data = pd.read_csv(newpath+'/'+filename, delimiter=',')
+    
+    # remove the '.csv' and underscores to get the state name
+    fn_clean = filename.replace('_',' ')
+    fn_clean = fn_clean.replace('.csv','')
+
+    # generate a variable
+    data['State'] = fn_clean
+
+    # write out the csv
+    data.to_csv(newpath+'/'+filename, index=False)
+
+# get all of our files into an object
+all_files = glob.glob(newpath + "/*.csv")
+
+# now append all of them
+master = pd.concat((pd.read_csv(f) for f in all_files))
+    
+# write out the master dataset
+data.to_csv('master.csv', index=False)
+
+# tar up the individual state data, just in case
+os.system("tar czfv states.tar.gz tempdata/")
+
+# remove the working directory
+os.system("rm -rf tempdata/")  
